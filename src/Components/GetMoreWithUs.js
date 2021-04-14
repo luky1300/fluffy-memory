f00731e099b45014bd23a6ec04e7ed3fdaf619a8
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-14 16:19:02 -0500

    add in and style the static heading and paragraph

--- src/Components/GetMoreWithUs.js
+++ src/Components/GetMoreWithUs.js
@@ -3,8 +3,8 @@ import '../style/GetMoreWithUs.css';
 
 function GetMoreWithUs() {
   return (
-    <section className="get-more-with-us-container">
-      <div>
+    <section className="get-more-with-us-main-container">
+      <div className="get-more-with-us-left-container">
         <h2>Get More With Us</h2>
         <div className="number-tabs">
           <p className="selected">01</p>
@@ -12,8 +12,14 @@ function GetMoreWithUs() {
           <p>03</p>
           <p>04</p>
         </div>
+        <h3>FREE APPS</h3>
+        <p className="get-more-with-us-text">
+          We regularly upload new free apps to our website, which is fully
+          accessible to our clients and subscribers. You can also find out about
+          free apps in our blog.
+        </p>
       </div>
-      <div className="get-more-with-us-img">
+      <div className="get-more-with-us-right-container">
         <img
           src="https://ld-wt73.template-help.com/wt_prod-18959/images/index-4-261x484.png"
           alt=""
