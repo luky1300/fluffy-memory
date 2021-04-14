9bb257acaaef913237f223fe871a0b9c0812c164
luky1300 <58443403+luky1300@users.noreply.github.com>
2021-04-14 18:20:22 -0400

    added what we offer to footer

--- src/Components/FooterComponents/Footer.js
+++ src/Components/FooterComponents/Footer.js
@@ -2,13 +2,14 @@ import React from "react";
 import "../../style/Footer.css";
 import ContactDetailsBlock from "./ContactDetailsBlock";
 import ContactForm from "./ContactForm";
+import WhatWeOffer from "./WhatWeOffer";
 
 function Footer() {
   return (
     <footer>
       <ContactDetailsBlock />
       <ContactForm /> 
-      What We Offer Block
+      <WhatWeOffer />
     </footer>
   );
 }
