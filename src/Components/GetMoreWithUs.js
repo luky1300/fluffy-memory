fdca9c959973e0147e119e315695273312849185
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-14 15:06:07 -0500

    initial css setup with flexbox

--- src/Components/GetMoreWithUs.js
+++ src/Components/GetMoreWithUs.js
@@ -3,8 +3,20 @@ import '../style/GetMoreWithUs.css';
 
 function GetMoreWithUs() {
   return (
-    <section>
-      <h2>Get More With Us</h2>
+    <section className="get-more-with-us-container">
+      <div>
+        <h2>Get More With Us</h2>
+      </div>
+      <div className="get-more-with-us-img">
+        <img
+          src="https://ld-wt73.template-help.com/wt_prod-18959/images/index-4-261x484.png"
+          alt=""
+        />
+        <img
+          src="https://ld-wt73.template-help.com/wt_prod-18959/images/index-5-261x484.png"
+          alt=""
+        />
+      </div>
     </section>
   );
 }
