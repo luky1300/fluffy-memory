d70468802e8cf0f7c91456d06755fe0c304967fb
luky1300 <58443403+luky1300@users.noreply.github.com>
2021-04-14 18:11:02 -0400

    added contact form to footer

--- src/Components/FooterComponents/Footer.js
+++ src/Components/FooterComponents/Footer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../../style/Footer.css";
 import ContactDetailsBlock from "./ContactDetailsBlock";
+import ContactForm from "./ContactForm";
 
 function Footer() {
   return (
     <footer>
       <ContactDetailsBlock />
-      Contact Form What We Offer Block
+      <ContactForm /> 
+      What We Offer Block
     </footer>
   );
 }
