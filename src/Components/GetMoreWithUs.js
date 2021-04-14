da583155dc5cb4cfab002ce119189f62e14a7c27
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-14 14:58:03 -0500

    created GetMoreWithUs js component and css. imported component in App.js

---
+++ src/Components/GetMoreWithUs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/GetMoreWithUs.css';
+
+function GetMoreWithUs() {
+  return (
+    <section>
+      <h2>Get More With Us</h2>
+    </section>
+  );
+}
+
+export default GetMoreWithUs;
