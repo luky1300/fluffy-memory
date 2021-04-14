6b5ad2a9c01d3aa77398d258f1cee8d45eec575d
luky1300 <58443403+luky1300@users.noreply.github.com>
2021-04-13 20:13:48 -0400

    created boiler plate

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Navbar</div>
+      <div>Carousel</div>
+      <div>All Services</div>
+      <div>Latest Projects</div>
+      <div>Achievements</div>
+      <div>Meet the Team</div>
+      <div>Get More with Us</div>
+      <div>What People Say</div>
+      <div>Latest Blog Posts</div>
+      <div>Footer</div>
+    </div>
+  );
+}
+
+export default App;
