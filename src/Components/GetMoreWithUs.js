be84de5168b6138c1cee41b192883592132a4f06
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-15 11:18:49 -0500

    tracking selected number tab on state

--- src/Components/GetMoreWithUs.js
+++ src/Components/GetMoreWithUs.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/GetMoreWithUs.css';
 
 function GetMoreWithUs() {
+  // tracks which number tab to highlight
+  const [selected, setSelected] = useState('01');
+  const numTabs = ['01', '02', '03', '04'];
   return (
     <section className="get-more-with-us-main-container">
       <div className="get-more-with-us-left-container">
         <h2>Get More With Us</h2>
         <div className="number-tabs">
-          <p className="selected">01</p>
-          <p>02</p>
-          <p>03</p>
-          <p>04</p>
+          {numTabs.map((num, index) => {
+            return (
+              <p
+                key={index}
+                className={selected === num ? 'selected' : ''}
+                onClick={() => setSelected(num)}
+              >
+                {num}
+              </p>
+            );
+          })}
         </div>
         <h3>FREE APPS</h3>
         <p className="get-more-with-us-text">
