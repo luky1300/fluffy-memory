0ea54dbfa2fe7c194dee0e7e7757b2ce2c2cb6b1
Caleb Matteis <matteismusic@gmail.com>
2021-04-21 06:52:22 -0400

    commit

---
+++ src/Components/CarouselComponents/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+    {
+      image:
+      'https://ld-wt73.template-help.com/wt_prod-18959/images/slider-1-slide-2-1770x742.jpg'
+    },
+    {
+      image:
+      'https://ld-wt73.template-help.com/wt_prod-18959/images/slider-1-slide-1-1770x742.jpg'
+    },
+    {
+      image:
+      'https://ld-wt73.template-help.com/wt_prod-18959/images/slider-1-slide-3-1770x742.jpg'
+    },
+];
