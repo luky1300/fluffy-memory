613a780c6076002b6d9aaa95ba0ee9fb55d0438c
Caleb Matteis <matteismusic@gmail.com>
2021-04-24 07:41:18 -0400

    commit

--- src/Components/CarouselComponents/Carousel.js
+++ src/Components/CarouselComponents/Carousel.js
@@ -1,3 +1,28 @@
 import React from 'react'
+import {
+    Background,
+  BackgroundHeader,
+  BackgroundHeader3,
+  Button,
 
+} from '../StyledComponents/CarouselStyled';
 
+function Carousel(){
+    return(
+
+        <Background>
+        <div>
+          <BackgroundHeader>Mobile App Development</BackgroundHeader>
+        <BackgroundHeader3>Since our establishment, we have been delivering high-quality and sustainable</BackgroundHeader3>
+        <BackgroundHeader3> software solutions for corporate purposes of worldwide businesses.</BackgroundHeader3>
+        <center>
+          <Button href="/">GET IN TOUCH</Button>
+        </center>
+        </div>
+        
+      </Background>
+
+    );
+};
+
+export default Carousel;
