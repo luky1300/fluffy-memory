2fbb9f018182f744fab8ad3a07c299f1faba30a5
jyoti <jshindems@gmail.com>
2021-04-28 15:03:10 -0400

    converted project component to styled component

--- src/Components/LatestProjects.js
+++ src/Components/LatestProjects.js
@@ -1,38 +1,46 @@
 import React from 'react';
 import '../style/LatestProjects.css';
-
+import {
+  Project,
+  Navigation,
+  NavigationLink,
+  NavList,
+  Image,
+  ProjectTitle,
+  ProjectDescription
+} from '../StyledComponents/LatestProjectsStyled'
 
 function LatestProjects(){
     return (
       <>
       <div>
-           <h1 className="project-title">Latest projects</h1>
-           <p className="project-text">
+           <ProjectTitle>Latest projects</ProjectTitle>
+           <ProjectDescription>
            In our portfolio, you can browse the latest products developed for our clients 
            for different corporate purposes. Our qualified team of interface designers 
            and software developers is always ready to create something unique for you.
-           </p>
+           </ProjectDescription>
       </div>
-      <div className="navigation">
-              <ul>
-                  <li>All</li>
-                  <li>Mobile App</li>
-                  <li>Custom Software</li>
-                  <li>QA & TESTING</li>
-                  <li>UX AND DESIGN</li>
-              </ul>
-      </div> 
+      <Navigation>
+              <NavigationLink>
+                  <NavList>All</NavList>
+                  <NavList>Mobile App</NavList>
+                  <NavList>Custom Software</NavList>
+                  <NavList>QA & TESTING</NavList>
+                  <NavList>UX AND DESIGN</NavList>
+              </NavigationLink>
+      </Navigation> 
       
-        <div className="project-container">
-        <img className="project-image" src="./img/grid-gallery-1.jpg" alt="FinStep" />
-        <img className="project-image" src="./img/grid-gallery-3.jpg" alt="MobileFinance"/>
-        <img className="project-image" src="./img/fullwidth-gallery-3-420x350.jpeg" alt="Q-Manage" />
-        <img className="project-image" src="./img/grid-gallery-6.jpg" alt="WeatherCast"/>
-        <img className="project-image" src="./img/projects04.jpeg" alt="FinStep"/>
-        <img className="project-image" src="./img/projects06.jpeg " alt="FinStep"/>
-        <img className="project-image" src="./img/grid-gallery-7.jpg " alt="FinStep"/>
-        <img className="project-image" src="./img/fullwidth-gallery-8-420x350.jpeg " alt="FinStep"/>
-        </div>
+        <Project>
+        <Image  src="./img/grid-gallery-1.jpg" alt="FinStep" />
+        <Image  src="./img/grid-gallery-3.jpg" alt="MobileFinance"/>
+        <Image  src="./img/fullwidth-gallery-3-420x350.jpeg" alt="Q-Manage" />
+        <Image  src="./img/grid-gallery-6.jpg" alt="WeatherCast"/>
+        <Image  src="./img/projects04.jpeg" alt="FinStep"/>
+        <Image  src="./img/projects06.jpeg " alt="FinStep"/>
+        <Image  src="./img/grid-gallery-7.jpg " alt="FinStep"/>
+        <Image  src="./img/fullwidth-gallery-8-420x350.jpeg " alt="FinStep"/>
+        </Project>
       </>
     );
 };
